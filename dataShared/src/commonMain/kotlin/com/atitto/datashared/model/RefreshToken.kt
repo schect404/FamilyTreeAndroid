030da12d3036eaa95cf8477fc91eaf6735b846db
Illia Tomilovych <illia.tomilovych@grandparade.co.uk>
2020-12-02 21:38:05 +0100

    Resolving version problems

--- dataShared/src/commonMain/kotlin/com/atitto/datashared/model/RefreshToken.kt
+++ dataShared/src/commonMain/kotlin/com/atitto/datashared/model/RefreshToken.kt
@@ -2,7 +2,6 @@ package com.atitto.datashared.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import com.atitto.mvi_kmm.stub.StubViewIntent
 
 @Serializable
 data class RefreshToken (
@@ -10,8 +9,4 @@ data class RefreshToken (
     val refreshToken: String?,
     @SerialName("grant_type")
     val grantType: String = "refresh_token"
-) {
-
-    fun newIntent() = StubViewIntent()
-
-}
+)
